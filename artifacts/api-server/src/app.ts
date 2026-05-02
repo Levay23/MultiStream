1ce7d62e39228ad8a9579e8e271fa6547e259675
Jorge Levay <levayjorge@gmail.com>
2026-05-02 02:48:31 -0500

    Final CORS fix: Move to top and remove unused import

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,10 +1,23 @@
 import express, { type Express } from "express";
-import cors from "cors";
+// import cors from "cors";
 import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://ventas-stream.web.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(
   pinoHttp({
@@ -25,18 +38,6 @@ app.use(
     },
   }),
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://ventas-stream.web.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
